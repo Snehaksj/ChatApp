85bae6bdcd514c9c1b1ab6177c31b4fb0f9c6552
Snehaksj <snehaksj@gmail.com>
2024-08-05 19:28:00 +0530

    removed unecessary node modules

--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import axiosInstance from "../Token/axiosInstance";
+import axiosInstance from "../../util/axiosInstance";
 
 const Page = () => {
   const [username, setUsername] = useState("");
